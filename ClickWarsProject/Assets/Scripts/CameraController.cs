c5b6c0a878af9c63b956f90763a5332b0000dd60
AlexRevolsdrib <68444995+AlexRevolsdrib@users.noreply.github.com>
2020-07-18 18:20:30 +0300

    Camera Controller

---
+++ ClickWarsProject/Assets/Scripts/CameraController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Renderer rend;
+    void Start() 
+    {
+        float orthoSize = rend.bounds.size.x * 13 * Screen.height / Screen.width * 0.5f;
+        Debug.Log(rend.bounds.size.x);
+        Camera.main.orthographicSize = orthoSize;
+    }
+
+
+}
